d9023297af78b2c65f251f7a2459eac84ae58b47
shreyaJain46 <141629858+shreyaJain46@users.noreply.github.com>
2024-03-04 07:09:44 +0530

    Update get_data_from_quizizz.sql

--- scripts/get_data_from_quizizz.sql
+++ scripts/get_data_from_quizizz.sql
@@ -134,6 +134,8 @@ WHERE
   quiz.is_deleted IS FALSE
   AND qv.is_cloned IS FALSE
   AND qv.is_draft IS FALSE
+  AND is_public IS TRUE
   AND (q.media.type = 'image' AND questionText is NOT NULL AND LENGTH(TRIM(questionText)) > 0 ) 
+  AND quiz_language = "English" 
 GROUP BY
   image
